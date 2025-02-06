57c216b40748ef3957d0fe13637a7efe01a29670
Hasan Can <130993691+HasanCan6241@users.noreply.github.com>
2025-02-06 21:45:01 +0300

    Update settings.py

--- techservice/techservice/settings.py
+++ techservice/techservice/settings.py
@@ -246,7 +246,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ulasgultekin6278@gmail.com'
-EMAIL_HOST_PASSWORD = 'jtgq rqbk qzxb ijiz'
+EMAIL_HOST_PASSWORD = ''
 
 
 # Default primary key field type
